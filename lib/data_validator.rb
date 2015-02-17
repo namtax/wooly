013bc84574ceae3ff9bd853cf30fd955e3a3522d
namtax <namtax@hotmail.com>
2015-02-17 18:48:40 +0000

    data_validator: format correctly

--- lib/data_validator.rb
+++ lib/data_validator.rb
@@ -1,36 +1,36 @@
 class DataValidator
-	attr_reader :input
+  attr_reader :input
 
-	EXPECTED_INPUT_SIZE = 9
+  EXPECTED_INPUT_SIZE = 9
 
-	def initialize(input)
-		@input = input
-	end
+  def initialize(input)
+    @input = input
+  end
 
-	def valid?
-		well_formed? &&
-		valid_size?  &&
-		valid_vote?  &&
-		valid_time?
-	end
+  def valid?
+    well_formed? &&
+    valid_size?  &&
+    valid_vote?  &&
+    valid_time?
+  end
 
-	private
+  private
 
-	def well_formed?
-		input
-			.force_encoding('UTF-8')
-			.valid_encoding?
-	end
+  def well_formed?
+    input
+      .force_encoding('UTF-8')
+      .valid_encoding?
+  end
 
-	def valid_size?
-		input.split.size == EXPECTED_INPUT_SIZE
-	end
+  def valid_size?
+    input.split.size == EXPECTED_INPUT_SIZE
+  end
 
-	def valid_vote?
-		input.split.first == 'VOTE'
-	end
+  def valid_vote?
+    input.split.first == 'VOTE'
+  end
 
-	def valid_time?
-		input.split[1].to_i != 0
-	end
+  def valid_time?
+    input.split[1].to_i != 0
+  end
 end
